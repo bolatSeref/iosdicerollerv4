f5147bda0b82b882257751df97ab9949a7e4c337
bolatSeref <bolat.seref.de@gmail.com>
2026-06-09 22:33:01 +0200

    files added

---
+++ DiceRollerv4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceRollerv4
+//
+//  Created by Test on 09.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceRollerv4/DiceRollerv4App.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerv4App.swift
+//  DiceRollerv4
+//
+//  Created by Test on 09.06.26.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerv4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
